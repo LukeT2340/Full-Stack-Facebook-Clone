ed5ef805466321c81881fd19faf05488dd7581d8
LukeT2340 <lukethompson220@gmail.com>
2024-05-25 20:46:51 +1000

    partially added messaging functionality

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -19,7 +19,7 @@ const Login = () => {
 // Left side of login page
 const LeftSide = () => {
     return (
-        <div className={`col-xl-4 col-lg-4 col-md-4 col-sm-8 col-8 mx-auto`}>
+        <div className={`col-xl-4 col-lg-4 col-md-4 col-8 mx-md-0 mx-auto`}>
             <h1 className={`${styles.appName}`}>headbook</h1>
             <p className={`${styles.blurb}`}>Headbook helps you connect and share with the people in your life.</p>
         </div>
