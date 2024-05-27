90cd7da792753d41c29f5c8858761ed57e9e00d0
LukeT2340 <lukethompson220@gmail.com>
2024-05-27 19:33:56 +1000

    Adjusted styles for the chat box

--- frontend/src/pages/Profile.js
+++ frontend/src/pages/Profile.js
@@ -102,7 +102,7 @@ const ProfileBottom = ({pageProfile, clientProfile}) => {
             <div className={`col-xl-7 col-lg-8 col-10`}>
                 <div className={`${styles.profileBottom} d-flex flex-column flex-md-row align-items-center`}>
                     {/* Left side */}
-                    <div className="col-xl-5 col-lg-4 col-md-10 col-12 mx-2">
+                    <div className="col-xl-5 col-lg-4 col-md-10 col-12 mx-2 mb-auto">
                         <Intro profile={pageProfile}/>
                     </div>
 
