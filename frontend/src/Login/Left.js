20b5ef96e53aec0dc5a4a3fb1c6635104f50d3a4
LukeT2340 <lukethompson220@gmail.com>
2024-05-29 21:32:00 +1000

    gave status media rounded borders

--- frontend/src/Login/Left.js
+++ frontend/src/Login/Left.js
@@ -3,7 +3,7 @@ import styles from './Login.module.css';
 // Left side of login page
 const Left = () => {
     return (
-        <div className={`col-xl-4 col-lg-4 col-md-4 col-8 mx-md-0 mx-auto`}>
+        <div className={`col-xl-4 col-lg-4 col-md-4 col-8 mx-md-0 my-3 mx-auto`}>
             <h1 className={`${styles.appName}`}>headbook</h1>
             <p className={`${styles.blurb}`}>Headbook helps you connect and share with the people in your life.</p>
         </div>
