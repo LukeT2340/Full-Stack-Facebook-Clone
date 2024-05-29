c350fecbc5c90b5d46854a18e8684aa0e42919d3
LukeT2340 <lukethompson220@gmail.com>
2024-05-29 20:13:58 +1000

    Made left section of the home page fixed

--- frontend/src/Home/Left.js
+++ frontend/src/Home/Left.js
@@ -7,7 +7,8 @@ import styles from "./Home.module.css"
 // Left column of the home page
 const Left = ({profile}) => {
     return (
-        <div className={`${styles.leftContainer} col-xl-2 col-lg-3 d-none d-lg-block mt-3 me-3`}>
+        <div className={`col-xl-2 col-lg-3 d-none d-lg-block mt-3 me-3`}>
+            <div className={styles.leftContainer} >
             <Nav.Link as={Link} to={`/profile/${profile._id}`} className={`${styles.profileContainer} m-0 p-2`}>
                 <img src={profile.profilePictureUrl} className={styles.optionImage} alt="profile picture"></img>
                 <p>{profile.firstName} {profile.lastName}</p>
@@ -28,6 +29,7 @@ const Left = ({profile}) => {
                 <FaCalendar className={styles.optionImage} color={'red'}/>
                 <p>Events</p>
             </Nav.Link>
+        </div>
         </div>
     )
 }
