6844418277224db5e1492274e2f912de7aa2198f
Luke Thompson <lukethompson@192-168-1-145.tpgi.com.au>
2024-07-15 10:29:34 +1000

    Added 'TODO' comment in Intro.js

--- frontend/src/Profile/Intro.js
+++ frontend/src/Profile/Intro.js
@@ -1,17 +1,27 @@
 import styles from "./Profile.module.css"
+import { useAuthContext } from '../hooks/useAuthContext'
 
 // Introduction
 const Intro = ({profile}) => {
+    const { user } = useAuthContext()
+
     return (
         <div className={`${styles.introContainer} me-md-4`}>
-            <h4>Intro</h4>
-            {profile.bio ? (
-                <button>Edit bio</button>
-             ) : (
-                <button>Add bio</button>
+            { /* TODO: This component should differ depeneding on whether it is the user's profile or not */}
+            {user._id === profile._id ? (
+              <>
+                <h4>Intro</h4>
+                {profile.bio ? (
+                    <button>Edit bio</button>
+                ) : (
+                    <button>Add bio</button>
+                )}
+                <button>Edit details</button>
+                <button>Add featured</button>
+            </>  
+            ) : (
+                <p>TODO: This component should differ depeneding on whether it is the user's profile or not</p>
             )}
-            <button>Edit details</button>
-            <button>Add featured</button>
         </div>
     )
 }
